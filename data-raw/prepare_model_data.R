3bdf77723512fbc9be90be9efe158aa842b3bc01
mrustl <michael.rustler@kompetenz-wasser.de>
2022-10-17 11:28:39 +0200

    Add boolean para to turn anonymisation on/off
    and also add "distance to surface water"

--- data-raw/prepare_model_data.R
+++ data-raw/prepare_model_data.R
@@ -1,8 +1,13 @@
+if (FALSE) {
+
+################################################################################
+### 2. Make initial model dataset (with 48 columns)
+################################################################################
 
 # load package, paths and variable sets from global.R --------------------------
-
 source("inst/extdata/scripts/global.R")
 
+anomynize_model_dataset <- TRUE
 
 # MAIN 1: get and prepare individual data sets ---------------------------------
 
@@ -45,7 +50,6 @@ df_W_static_sd <- get_W_static_data(paths$data_W_static, renamings, df_wells) %>
   dplyr:::group_by(well_id) %>%
   dplyr::summarise(W_static.sd = sd(W_static, na.rm = TRUE))
 
-
 # MAIN 2: combine individual data sets -----------------------------------------
 
 # 1. well data + surface water + drilling + quality (-> feature table)
@@ -63,7 +67,6 @@ well_feature_data <- df_wells %>%
                 volume_m3_d.cv = replace_na_with_median(volume_m3_d.cv)) %>%
   droplevels()
 
-
 # 2. combine pump test data and capacity measurements (virtual pump_tests)
 df_pump_test_Q_monitoring <-
   dwc.wells:::combine_pump_test_and_Q_monitoring_data(
@@ -71,11 +74,12 @@ df_pump_test_Q_monitoring <-
     df_Q_monitoring
   )
 
+# 3. combine Qs and well_features (in total: 48 columns)
+model_data <- dwc.wells:::combine_to_model_data(df_pump_tests_tidy,
+                                                well_feature_data) %>%
+  anonymize_dataset(anomynize = anomynize_model_dataset)
 
-# 3. combine Qs and well_features
-model_data <- dwc.wells:::combine_to_model_data(df_pump_tests_tidy, well_feature_data)
 save_data(model_data, paths$data_prep_out, "model_data")
-
 
 ### Check different compression formats (as recommended by Rcmdcheck):
 ### https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Data-in-packages
@@ -84,92 +88,95 @@ save_data(model_data, paths$data_prep_out, "model_data")
 #usethis::use_data(model_data, compress = "bzip2", overwrite = TRUE) # 261 kB
 usethis::use_data(model_data, compress = "xz", overwrite = TRUE) # 180 kB
 
+################################################################################
+### 2. Make reduced model dataset (based on "data analysis" findings),
+###    (with 27 columns)
+################################################################################
+model_data_reduced <- reduce_model_dataset(model_data)
+
+usethis::use_data(model_data_reduced, compress = "xz", overwrite = TRUE) # 180 kB
+}
 
 
-# load data --------------------------------------------------------------------
+reduce_model_dataset <- function(model_data) {
 
-# load(file.path(paths$data_prep_out, "model_data.RData"))
-model_data_reduced <- model_data %>%
-  dplyr::select(.data$Qs_rel,
-                .data$well_id,
-                tidyselect::all_of(model_features))
-
-
-# refine variable selection ----------------------------------------------------
-
-
-# remove correlated and unimportant variables (see "inst/extdata/scripts/variable_importance.R")
-
-if (TRUE) {
-
+  model_data %>%
+    dplyr::select(.data$Qs_rel,
+                  .data$well_id,
+                  tidyselect::all_of(model_features)) %>%
+  # remove correlated and unimportant variables (see "inst/extdata/scripts/variable_importance.R")
   # remove correlated variables
-  model_data_reduced  <- model_data_reduced  %>%
     dplyr::select(-c(well_depth, quality.DR, quality.P_tot,
-              volume_m3_d.sd, waterworks, surface_water))
-
+                     volume_m3_d.sd, waterworks, surface_water)) %>%
   # remove unimportant variables
-  model_data_reduced  <- model_data_reduced  %>%
-    dplyr::select(-c(n_screens, filter_length, quality.Cu, inliner))
-
+    dplyr::select(-c(n_screens, filter_length, quality.Cu, inliner)) %>%
   # remove well gallery (local variable which makes models not applicable
   # to new sites or well galleries)
-  model_data_reduced <- model_data_reduced %>%
     dplyr::select(-well_gallery)
-
-
-    ### remove non ASCII character
-    is_spuehlbohrung <- grepl(pattern = "^Sp.*bohrung$",
-                              x = model_data_reduced$drilling_method)
-
-   model_data_reduced$drilling_method <- as.character(model_data_reduced$drilling_method)
-
-
-   model_data_reduced$drilling_method[is_spuehlbohrung] <- "Spuehlbohrung"
-
-
-   model_data_reduced$drilling_method <- as.factor(model_data_reduced$drilling_method)
-
-   model_data_reduced$aquifer_coverage <- kwb.utils::multiSubstitute(
-     strings = model_data_reduced$aquifer_coverage,
-     replacements = list(`bedeckt` = "confined",
-                         `unbedeckt` = "unconfined",
-                         `Unbekannt` = "unknown",
-                         `bedeckt` = "confined",
-                         `randlich` = "edges",
-                         `teilweise` = "partly")
-     ) %>%
-     as.factor()
-
-   anonymize_hash <- function(x, algo="crc32"){
-
-     unq_hashes <- vapply(unique(x),
-                          function(object) digest::digest(object, algo=algo),
-                          FUN.VALUE="",
-                          USE.NAMES=TRUE)
-
-     unname(unq_hashes[x])
-   }
-
-   anonymize_int <- function(x, n_values = 1000000){
-
-     values <- x
-     unique_values <- unique(values)
-     n_unique_values <- as.integer(length(unique_values))
-     random_values <- sample.int(n = n_values,
-                                 size = n_unique_values,
-                                 replace = FALSE)
-
-     ### https://stackoverflow.com/a/50898859
-     as.integer(as.character(factor(values, unique_values, random_values)))
-   }
-
-   set.seed(1)
-   model_data_reduced$well_id <- anonymize_int(model_data_reduced$well_id)
-   model_data_reduced$screen_material <- as.factor(anonymize_hash(model_data_reduced$screen_material))
-   model_data_reduced$drilling_method <- as.factor(anonymize_hash(model_data_reduced$drilling_method))
 
 }
 
-usethis::use_data(model_data_reduced, compress = "xz", overwrite = TRUE) # 180 kB
+
+anonymize_hash <- function(x, algo="crc32"){
+
+  unq_hashes <- vapply(unique(x),
+                       function(object) digest::digest(object, algo=algo),
+                       FUN.VALUE="",
+                       USE.NAMES=TRUE)
+
+  unname(unq_hashes[x])
+}
+
+anonymize_int <- function(x, n_values = 1000000){
+
+  values <- x
+  unique_values <- unique(values)
+  n_unique_values <- as.integer(length(unique_values))
+  random_values <- sample.int(n = n_values,
+                              size = n_unique_values,
+                              replace = FALSE)
+
+  ### https://stackoverflow.com/a/50898859
+  as.integer(as.character(factor(values, unique_values, random_values)))
+}
 
 
+anonymize_dataset <- function(df, anomynize = TRUE, seed_number = 1) {
+
+  if(anomynize) {
+    stopifnot(all(c("aquifer_coverage",
+                    "drilling_method",
+                    "screen_material",
+                    "surface_water.distance",
+                    "well_id") %in% names(df)))
+    set.seed(seed = seed_number)
+    df$aquifer_coverage <- as.factor(anonymize_hash(df$aquifer_coverage))
+    df$drilling_method <- as.factor(anonymize_hash(df$drilling_method))
+    df$screen_material <- as.factor(anonymize_hash(df$screen_material))
+    df$surface_water.distance <- as.factor(anonymize_hash(df$surface_water.distance))
+    df$well_id <- anonymize_int(df$well_id)
+  }
+
+  df
+
+}
+
+
+### currently not used
+translate_aquifer_coverage <- function(stings, translate = TRUE) {
+
+  if(translate) {
+    strings <- kwb.utils::multiSubstitute(
+      strings = strings,
+      replacements = list(`bedeckt` = "confined",
+                          `unbedeckt` = "unconfined",
+                          `Unbekannt` = "unknown",
+                          `bedeckt` = "confined",
+                          `randlich` = "edges",
+                          `teilweise` = "partly")
+    ) %>%
+      as.factor()
+  }
+
+  strings
+}
